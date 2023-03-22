5fd3cd5438688b43c31f56a2b6c326a99184d6bc
lexatang <t@tcj.ac.cn>
2023-03-22 16:42:28 +0800

    fix: add package umi4 to avoid using ts-ignore

--- src/plugins/umi/react-inspector.ts
+++ src/plugins/umi/react-inspector.ts
@@ -2,14 +2,14 @@
  * preset plugins for umi3
  */
 
-import type { IApi } from '@umijs/types'
+import type { IApi as IApi3 } from '@umijs/types'
+import type { IApi as IApi4 } from 'umi4'
 import {
   createLaunchEditorMiddleware,
-} from '../webpack/middlewares'
-import type { InspectorPluginOptions } from '../babel'
+} from '../webpack'
 
-export default function inspectorPlugin(api: IApi) {
-  const inspectorConfig = api.userConfig.inspectorConfig as InspectorPluginOptions | undefined
+export default function inspectorPlugin(api: IApi3 | IApi4) {
+  const inspectorConfig = api.userConfig.inspectorConfig
 
   api.describe({
     key: 'inspectorConfig',
@@ -18,14 +18,11 @@ export default function inspectorPlugin(api: IApi) {
         return joi.object()
       },
     },
-    enableBy: api.EnableBy.register,
   })
-
-  // umi4
-  // @ts-ignore
-  if (api.addBeforeBabelPlugins) {
-    // @ts-ignore
-    api.addBeforeBabelPlugins(() => [
+  if ((api as IApi4).addBeforeBabelPlugins) {
+    const api4 = api as IApi4
+    api.logger.info(`Using react-dev-inspector Umi4 plugin with Umi ${process.env.UMI_VERSION}.`)
+    api4.addBeforeBabelPlugins(() => [
       [
         require.resolve('@react-dev-inspector/babel-plugin'),
         {
@@ -37,9 +34,11 @@ export default function inspectorPlugin(api: IApi) {
         },
       ],
     ])
-  } else {
-    // umi3
-    api.modifyBabelOpts((babelOptions) => {
+    api4.addBeforeMiddlewares(createLaunchEditorMiddleware)
+  } else if ((api as IApi3).modifyBabelOpts) {
+    const api3 = api as IApi3
+    api.logger.info(`Using react-dev-inspector Umi3 plugin with Umi ${process.env.UMI_VERSION}.`)
+    api3.modifyBabelOpts((babelOptions) => {
       babelOptions.plugins.unshift([
         'react-dev-inspector/plugins/babel',
         {
@@ -52,13 +51,7 @@ export default function inspectorPlugin(api: IApi) {
       ])
       return babelOptions
     })
+    api3.addBeforeMiddewares(createLaunchEditorMiddleware)
   }
 
-  // umi4 & umi3
-  if (api.addBeforeMiddlewares) {
-    api.addBeforeMiddlewares(createLaunchEditorMiddleware)
-  } else {
-    // legacy umi3
-    api.addBeforeMiddewares(createLaunchEditorMiddleware)
-  }
 }
